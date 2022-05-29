b798b29b7e98d84f051c9686f89be3b1c40ab152
honghoker <ekflzhftk@naver.com>
2022-05-29 21:00:10 +0900

    0529 BOJ

---
+++ CodingTest/CodingTest/BOJ/22.05.29/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  CodingTest
+//
+//  Created by 홍은표 on 2022/05/29.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+// 11726 2×n 타일링 풀이중
+
+// 세로 가로
+// 2 x 1 = 2x1 한개 : 1
+// 2 x 2 = 1x2 두개, 2x1 두개 : 2
+// 2 x 3 = 2x1 세개, 1x2 6개,
+
+
+// 10007
+// 20014
