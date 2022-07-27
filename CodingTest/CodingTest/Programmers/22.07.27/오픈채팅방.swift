35fbafb45022a72b733ab5db0438281c94929ae5
honghoker <ekflzhftk@naver.com>
2022-07-27 19:45:37 +0900

    progammers 2019 KAKAO BLIND RECRUITMENT 오픈채팅방

--- CodingTest/CodingTest/Programmers/22.07.27/오픈채팅방.swift
+++ CodingTest/CodingTest/Programmers/22.07.27/오픈채팅방.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+func solution(_ record:[String]) -> [String] {
+    var nickName = [String: String]()
+    var result = [(String, String)]()
+    
+    for i in 0..<record.count {
+        let sun = record[i].split(separator: " ").map{String($0)}
+        
+        let (state, uid) = (sun[0], sun[1])
+        if state == "Enter" {
+            nickName[uid] = sun[2]
+            result.append((uid, "님이 들어왔습니다."))
+        } else if state == "Leave" {
+            result.append((uid, "님이 나갔습니다."))
+        } else {
+            nickName[uid] = sun[2]
+        }
+    }
+
+    return result.map { "\(nickName[$0.0]!)\($0.1)" }
+}
