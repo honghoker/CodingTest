f0ca7b99bbaf1484f3e94b4a5312fbefc6755f01
honghoker <ekflzhftk@naver.com>
2022-07-27 20:21:27 +0900

    progammers 2017 팁스타운 짝지어제거하기

--- CodingTest/CodingTest/Programmers/22.07.27/짝지어 제거하기.swift
+++ CodingTest/CodingTest/Programmers/22.07.27/짝지어 제거하기.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+func solution(_ s:String) -> Int {
+    var stack = [Character]()
+    var str = Array(s)
+    
+    for i in 0..<str.count {
+        if stack.isEmpty { stack.append(str[i]) }
+        else {
+            if stack.last! == str[i] { stack.removeLast() }
+            else { stack.append(str[i]) }
+        }
+    }
+
+    return stack.isEmpty ? 1 : 0
+}
