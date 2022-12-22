5caab0fa193ec0b766be0573387cb6deeb2464ce
honghoker <ekflzhftk@naver.com>
2022-12-23 01:58:07 +0900

    [BOJ] 11724번 연결요소의 개수 재풀이

--- CodingTest/CodingTest/BOJ/22.06.12/11724번 연결요소의 개수.swift
+++ CodingTest/CodingTest/BOJ/22.06.12/11724번 연결요소의 개수.swift
@@ -1,22 +1,32 @@
-//
-//  11724번 연결요소의 개수
-//  CodingTest
-//
+//  백준 - 연결요소의 개수
+//  https://www.acmicpc.net/problem/11724
 //  Created by 홍은표 on 2022/06/12.
-//
 
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-let (N, M) = (input[0], input[1])
+var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0; buffer.append(0)
 
+@inline(__always) func readByte() -> UInt8 {
+    defer { byteIdx += 1 }
+    return buffer.withUnsafeBufferPointer { $0[byteIdx] }
+}
+
+@inline(__always) func readInt() -> Int {
+    var number = 0, byte = readByte(), isNegative = false
+    while byte == 10 || byte == 32 { byte = readByte() }
+    if byte == 45 { byte = readByte(); isNegative = true }
+    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+    return number * (isNegative ? -1 : 1)
+}
+
+let (N, M) = (readInt(), readInt())
 var arr: [[Int]] = Array(repeating: [], count: N + 1)
 var visit = Array(repeating: false, count: N + 1)
 
 for _ in 0..<M {
-    let node = readLine()!.split(separator: " ").map { Int(String($0))! }
-    arr[node[0]].append(node[1])
-    arr[node[1]].append(node[0])
+    let (u, v) = (readInt(), readInt())
+    arr[u].append(v)
+    arr[v].append(u)
 }
 
 var answer = 0
@@ -25,9 +35,7 @@ func dfs(_ now: Int) {
     visit[now] = true
     for i in 0..<arr[now].count {
         let next = arr[now][i]
-        if visit[next] == false {
-            dfs(next)
-        }
+        if visit[next] == false { dfs(next) }
     }
 }
 
@@ -39,25 +47,3 @@ for i in 1...N {
 }
 
 print(answer)
-
-//for i in 1...N {
-//    if visit[i] == false {
-//        visit[i] = true
-//        var stack = [i]
-//        answer += 1
-//
-//        while !stack.isEmpty {
-//            let node = stack.removeLast()
-//            let temp = arr[node]
-//            for j in 0..<temp.count {
-//                let next = temp[j]
-//                if visit[next] == false {
-//                    visit[next] = true
-//                    stack.append(next)
-//                }
-//            }
-//        }
-//    }
-//}
-
-
