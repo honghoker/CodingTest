3a342202057f2ecda4dfbc7c01a27472854a7bc0
honghoker <ekflzhftk@naver.com>
2023-01-29 17:56:46 +0900

    [BOJ] 1202번 보석 도둑

---
+++ CodingTest/CodingTest/BOJ/23.01.29/1202번 보석 도둑.swift
@@ -0,0 +1,192 @@
+//  백준 - 보석 도둑
+//  https://www.acmicpc.net/problem/1202
+//  Created by 홍은표 on 2023/01/29.
+
+import Foundation
+
+//  Written for the Swift Algorithm Club by Kevin Randrup and Matthijs Hollemans
+public struct Heap<T> {
+    var nodes = [T]()
+    
+    // Use '>' for a max-heap or '<' for a min-heap,
+    private var orderCriteria: (T, T) -> Bool
+
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+    
+    public init(array: [T], sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+        configureHeap(from: array)
+    }
+ 
+    private mutating func configureHeap(from array: [T]) {
+        nodes = array
+        for i in stride(from: (nodes.count/2-1), through: 0, by: -1) {
+            shiftDown(i)
+        }
+    }
+    
+    public var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+    
+    public var count: Int {
+        return nodes.count
+    }
+    
+    @inline(__always) internal func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+    
+    @inline(__always) internal func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+
+    @inline(__always) internal func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+
+    public func peek() -> T? {
+        return nodes.first
+    }
+
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+
+    public mutating func insert<S: Sequence>(_ sequence: S) where S.Iterator.Element == T {
+        for value in sequence {
+            insert(value)
+        }
+    }
+
+    public mutating func replace(index i: Int, value: T) {
+        guard i < nodes.count else { return }
+        
+        remove(at: i)
+        insert(value)
+    }
+
+    @discardableResult public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+        
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+    
+    @discardableResult public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+        
+        let size = nodes.count - 1
+        if index != size {
+            nodes.swapAt(index, size)
+            shiftDown(from: index, until: size)
+            shiftUp(index)
+        }
+        return nodes.removeLast()
+    }
+
+    internal mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: childIndex)
+        
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+        
+        nodes[childIndex] = child
+    }
+
+    internal mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+   
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+        
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+    
+    internal mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+}
+
+// MARK: - Searching
+extension Heap where T: Equatable {
+    public func index(of node: T) -> Int? {
+        return nodes.firstIndex(where: { $0 == node })
+    }
+    
+    @discardableResult public mutating func remove(node: T) -> T? {
+        if let index = index(of: node) {
+            return remove(at: index)
+        }
+        return nil
+    }
+}
+
+var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0; buffer.append(0)
+
+@inline(__always) func readByte() -> UInt8 {
+    defer { byteIdx += 1 }
+    return buffer.withUnsafeBufferPointer { $0[byteIdx] }
+}
+
+@inline(__always) func readInt() -> Int {
+    var number = 0, byte = readByte(), isNegative = false
+    while byte == 10 || byte == 32 { byte = readByte() }
+    if byte == 45 { byte = readByte(); isNegative = true }
+    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+    return number * (isNegative ? -1 : 1)
+}
+
+let (N, K) = (readInt(), readInt())
+
+var jewelries: [(Int, Int)] = []
+var bags: [Int] = []
+
+for _ in 0..<N { jewelries.append((readInt(), readInt())) }
+for _ in 0..<K { bags.append(readInt()) }
+
+// 보석 무게와 가방 무게를 오름차순 정렬
+jewelries.sort(by: { $0.0 < $1.0 }); bags.sort()
+
+// 보석 가격이 비싼 순으로 정렬하는 maxHeap
+var maxHeap = Heap(array: [(Int, Int)](), sort: { $0.1 > $1.1 })
+var answer = 0, index = 0
+
+for bag in bags {
+    // 보석이 현재 가방에 들어간다면
+    while index < jewelries.count && jewelries[index].0 <= bag {
+        // maxHeap에 보석 insert
+        maxHeap.insert(jewelries[index])
+        index += 1
+    }
+    
+    // maxHeap이 비지않았다면 현재 가방에 들어갈 수 있는 보석이 있다는 것.
+    if !maxHeap.isEmpty {
+        // 가장 비싼 보석을 가방에 넣는다.
+        answer += maxHeap.remove()!.1
+    }
+}
+
+print(answer)
