ef32a646d289e288ddf868ccb2078189306534cc
honghoker <ekflzhftk@naver.com>
2023-12-17 20:40:13 +0900

    Update MinHeap

--- CodingTest/CodingTest/ETC/MinHeap.swift
+++ CodingTest/CodingTest/ETC/MinHeap.swift
@@ -3,97 +3,78 @@
 
 import Foundation
 
-struct MinHeap<T: Comparable> {
-    private var heap: Array<T> = []
-    var isEmpty: Bool { heap.count <= 1 }
-    var count: Int { heap.count - 1 }
-    
-    init() { }
-    init(_ data: T) {
-        heap.append(data)       // 0번 index 채우기용
-        heap.append(data)       // 실제 Root Node 채우기
+struct MinHeap<Element: Comparable> {
+    var elements: [Element] = []
+
+    var isEmpty: Bool {
+        return elements.isEmpty
     }
-    
-    mutating func insert(_ data: T) {
-        if heap.isEmpty {
-            heap.append(data)
-            heap.append(data)
-            return
-        }
-        heap.append(data)
-        
-        func isMoveUp(_ insertIndex: Int) -> Bool {
-            if insertIndex <= 1 {               // 루트 노드일 때
-                return false
-            }
-            let parentIndex: Int = insertIndex / 2
-            return heap[insertIndex] < heap[parentIndex] ? true : false
-        }
-        
-        var insertIndex: Int = heap.count - 1
-        while isMoveUp(insertIndex) {
-            let parentIndex: Int = insertIndex / 2
-            heap.swapAt(insertIndex, parentIndex)
-            insertIndex = parentIndex
+
+    var count: Int {
+        return elements.count
+    }
+
+    mutating func insert(_ element: Element) {
+        elements.append(element)
+        siftUp(from: elements.count - 1)
+    }
+
+    mutating private func siftUp(from index: Int) {
+        var child = index
+        var parent = parentIndex(of: child)
+
+        while child > 0 && elements[child] < elements[parent] {
+            elements.swapAt(child, parent)
+            child = parent
+            parent = parentIndex(of: child)
         }
     }
-    
-    enum moveDownStatus { case left, right, none }
-    
-    mutating func pop() -> T? {
-        if heap.count <= 1 { return nil }
-        
-        let returnData = heap[1]
-        heap.swapAt(1, heap.count - 1)
-        heap.removeLast()
-        
-        func moveDown(_ poppedIndex: Int) -> moveDownStatus {
-            let leftChildIndex = (poppedIndex * 2)
-            let rightCildIndex = leftChildIndex + 1
-            
-            // case1. 모든(왼쪽) 자식 노드가 없는 경우 (완전이진트리는 왼쪽부터 채워지므로)
-            if leftChildIndex >= (heap.count) {
-                return .none
-            }
-            
-            // case2. 왼쪽 자식 노드만 있는 경우
-            if rightCildIndex >= (heap.count) {
-                return heap[leftChildIndex] < heap[poppedIndex] ? .left : .none
-            }
-            
-            // case3. 왼쪽 & 오른쪽 자식 노드 모두 있는 경우
-            // case 3-1. 자식들이 자신보다 모두 작은 경우
-            if (heap[leftChildIndex] > heap[poppedIndex]) && (heap[rightCildIndex] > heap[poppedIndex]) {
-                return .none
-            }
-            
-            // case 3-2. 자식들이 자신보다 모두 큰 경우 (왼쪽과 오른쪽 자식 중 더 큰 자식 선별)
-            if (heap[leftChildIndex] < heap[poppedIndex]) && (heap[rightCildIndex] < heap[poppedIndex]) {
-                return heap[leftChildIndex] < heap[rightCildIndex] ? .left : .right
-            }
-            
-            // case3-3. 왼쪽과 오른쪽 자식 중, 한 자식만 자신보다 작은 경우
-            if (heap[leftChildIndex] < heap[poppedIndex]) || (heap[rightCildIndex] < heap[poppedIndex]) {
-                return heap[leftChildIndex] < heap[rightCildIndex] ? .left : .right
-            }
-            return .none
+
+    mutating func pop() -> Element? {
+        guard !isEmpty else {
+            return nil
         }
-        
-        var poppedIndex = 1
+
+        elements.swapAt(0, count - 1)
+        let element = elements.removeLast()
+        siftDown(from: 0)
+        return element
+    }
+
+    mutating private func siftDown(from index: Int) {
+        var parent = index
+
         while true {
-            switch moveDown(poppedIndex) {
-            case .none:
-                return returnData
-            case .left:
-                let leftChildIndex = poppedIndex * 2
-                heap.swapAt(poppedIndex, leftChildIndex)
-                poppedIndex = leftChildIndex
-            case .right:
-                let rightChildIndex = (poppedIndex * 2) + 1
-                heap.swapAt(poppedIndex, rightChildIndex)
-                poppedIndex = rightChildIndex
-                
+            let leftChild = leftChildIndex(of: parent)
+            let rightChild = rightChildIndex(of: parent)
+            var candidate = parent
+
+            if leftChild < count && elements[leftChild] < elements[candidate] {
+                candidate = leftChild
             }
+
+            if rightChild < count && elements[rightChild] < elements[candidate] {
+                candidate = rightChild
+            }
+
+            if candidate == parent {
+                return
+            }
+
+            elements.swapAt(parent, candidate)
+            parent = candidate
         }
+    }
+
+    private func parentIndex(of index: Int) -> Int {
+        return (index - 1) / 2
+    }
+
+    private func leftChildIndex(of index: Int) -> Int {
+        return index * 2 + 1
+    }
+
+    private func rightChildIndex(of index: Int) -> Int {
+        return index * 2 + 2
     }
 }
